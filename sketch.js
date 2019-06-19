f8a3a99e1cc967cf2374db7f31a064eaf4772c2b
jonny <ga86lad@mytum.de>
2019-06-19 20:34:05 +0200

    extrackt track generation extract checkpoint amount

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ const MUTATION_RATE = 0.1;
 const LIFESPAN = 25;
 const SIGHT = 50;
 
+const TRACK_CHECKPOINT_AMOUNT = 60;
+
 let generationCount = 0;
 
 let walls = [];
@@ -32,52 +34,16 @@ let checkpoints = [];
 // when any of the particle completes multiple rounds in current map
 // this will help to make the current generation to work on new map
 // and generalize to variety of maps
-const maxFitness = 500;
+const maxFitness = TRACK_CHECKPOINT_AMOUNT * 5;
 let changeMap = false;
 
-function buildTrack() {
-  checkpoints = [];
-  inside = [];
-  outside = [];
 
-  let noiseMax = 4;
-  const total = 60;
-  const pathWidth = 60;
-  let startX = random(1000);
-  let startY = random(1000);
-  for (let i = 0; i < total; i++) {
-    let a = map(i, 0, total, 0, TWO_PI);
-    let xoff = map(cos(a), -1, 1, 0, noiseMax) + startX;
-    let yoff = map(sin(a), -1, 1, 0, noiseMax) + startY;
-    let xr = map(noise(xoff, yoff), 0, 1, 100, width * 0.5);
-    let yr = map(noise(xoff, yoff), 0, 1, 100, height * 0.5);
-    let x1 = width / 2 + (xr - pathWidth) * cos(a);
-    let y1 = height / 2 + (yr - pathWidth) * sin(a);
-    let x2 = width / 2 + (xr + pathWidth) * cos(a);
-    let y2 = height / 2 + (yr + pathWidth) * sin(a);
-    checkpoints.push(new Boundary(x1, y1, x2, y2));
-    inside.push(createVector(x1, y1));
-    outside.push(createVector(x2, y2));
-  }
-  walls = [];
-  for (let i = 0; i < checkpoints.length; i++) {
-    let a1 = inside[i];
-    let b1 = inside[(i + 1) % checkpoints.length];
-    walls.push(new Boundary(a1.x, a1.y, b1.x, b1.y));
-    let a2 = outside[i];
-    let b2 = outside[(i + 1) % checkpoints.length];
-    walls.push(new Boundary(a2.x, a2.y, b2.x, b2.y));
-  }
-
-  start = checkpoints[0].midpoint();
-  end = checkpoints[checkpoints.length - 1].midpoint();
-}
 
 function setup() {
   createCanvas(1200, 800);
 
   tf.setBackend('cpu');
-  buildTrack();
+  buildTrack(TRACK_CHECKPOINT_AMOUNT);
   // let a = inside[inside.length - 1];
   // let b = outside[outside.length - 1];
   // walls.push(new Boundary(a.x, a.y, b.x, b.y));
@@ -124,14 +90,14 @@ function draw() {
         savedParticles.push(population.splice(i, 1)[0]);
       }
 
-      buildTrack();
+      buildTrack(TRACK_CHECKPOINT_AMOUNT);
       nextGeneration();
       generationCount++;
       changeMap = false;
     }
 
     if (population.length == 0) {
-      buildTrack();
+      buildTrack(TRACK_CHECKPOINT_AMOUNT);
       nextGeneration();
       generationCount++;
     }
